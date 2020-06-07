13cd5cf4fb713c759ba56006f54fd6a431e148bc
watermelon892 <sato.taro.892@gmail.com>
2020-06-07 15:46:17 +0900

    Add a 06d

---
+++ 06/06d/c_src/int.c
@@ -0,0 +1,21 @@
+/* Interrupt */
+
+#include "bootpack.h"
+
+void init_pic(void) {
+  io_out8(PIC0_IMR, 0xff);    // Do not accept all interrupts
+  io_out8(PIC1_IMR, 0xff);    // Do not accept all interrupts
+
+  io_out8(PIC0_ICW1, 0x11);   // Edge trigger mode
+  io_out8(PIC0_ICW2, 0x20);   // IRQ0-7 is INT20-27
+  io_out8(PIC0_ICW3, 1 << 2); // PIC1 is connected by IRQ2
+  io_out8(PIC0_ICW4, 0x01);   // Non-buffer mode
+
+  io_out8(PIC1_ICW1, 0x11);   // Edge trigger mode
+  io_out8(PIC1_ICW2, 0x28);   // IRQ8-15 is INT28-2f
+  io_out8(PIC1_ICW3, 2);      // PIC1 is connected by IRQ2
+  io_out8(PIC1_ICW4, 0x01);   // Non-buffer mode
+
+  io_out8(PIC0_IMR, 0xfb);    // 11111011 : All except PIC1 prohibited
+  io_out8(PIC1_IMR, 0xff);    // 11111111 : Not accept all interrupts
+}
--- 06/README.md
+++ 06/README.md
@@ -1,6 +1,6 @@
 ## Demo
 
-06a - 06b
+06a - 06d
 
 ![template](https://github.com/watermelon892/OSPractice/blob/master/06/pic/06a.png)
 
