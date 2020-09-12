7881d2b793fa46287ff5f813c43b93eeb499e64d
sato <sato.taro.892@gmail.com>
2020-09-12 14:34:35 +0900

    Add a 19a

---
+++ 19_Application/19a/lib/my_clib.h
@@ -0,0 +1,12 @@
+#ifndef __MY_CLIB_H__
+#define __MY_CLIB_H__
+
+#include <stdarg.h>
+
+#define TRUE  1
+#define FALSE 0
+
+int my_sprintf(char *str, char *format, ...);
+int my_strcmp(char *str1, char *str2);
+
+#endif // __MY_CLIB_H__
---
+++ 19_Application/README.md
@@ -0,0 +1,14 @@
+## Demo
+
+19a
+
+type command
+
+![template](https://github.com/watermelon892/OSPractice/blob/master/19_Application/pic/19a.png)
+
+## Usage
+
+```
+$ make
+$ make qemu
+```
